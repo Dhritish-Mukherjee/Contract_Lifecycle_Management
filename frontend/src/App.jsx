2cb18d4ad34696491cbef915772440bdeec3c732
Codewithpabitra <pabitra05.maity@gmail.com>
2026-01-15 00:41:53 +0530

    add login page and motion to it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Home from './pages/Home'
+import { Route, Routes } from 'react-router-dom'
+import Layout from './pages/User/Layout';
+import Login from './pages/Auth/Login';
 
 const App = () => {
+  const token = false;
+
   return (
     <div>
-        <Home />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/admin' element={token ? <Layout /> : <Login />} />
+        </Routes>
     </div>
   )
 }
